4e52b09025e7f6d9f4d83eb3001904f9080db426
Erik Yang <erikyang@Chisius-MacBook-Air.local>
2017-02-27 02:31:41 -0800

    finished hangman project

--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -65,9 +65,30 @@ class HangmanViewController: UIViewController {
         return result
     }
     
+    @IBOutlet weak var hangmanImage: UIImageView!
+    func updateHangmanImage(){
+        let num = incorrectGuesses.count
+        if(num==0){
+            hangmanImage.image = UIImage(named: "hangman1")
+        }
+        if(num<=6){
+            let imgString: String = "hangman" + String(num+1)
+            hangmanImage.image = UIImage(named: imgString)
+        }
+    }
+    
     @IBOutlet weak var wordLabel: UILabel!
     func updateWordLabel(result: String){
         self.wordLabel.text = result
+    }
+    
+    @IBOutlet weak var incorrectGuessesLabel: UILabel!
+    func updateIncorrectGuessesLabel(){
+        var result = "Incorrect Guesses: ";
+        for letter in incorrectGuesses{
+            result += letter + " "
+        }
+        self.incorrectGuessesLabel.text = result
     }
     
     @IBOutlet weak var guessTextField: UITextField!
@@ -76,11 +97,65 @@ class HangmanViewController: UIViewController {
         // ?? notation - default value is ""
         var guess:String = guessTextField.text ?? ""
         if(guess.characters.count==1){
-            if(!guesses.contains(guess.uppercased())){
+            if(guess != " " && !guesses.contains(guess.uppercased())){
                 guesses.append(guess.uppercased())
+                if(!answer.contains(guess.uppercased())){
+                    incorrectGuesses.append(guess.uppercased())
+                }
                 updateWordLabel(result: answerToBlanks())
+                updateIncorrectGuessesLabel()
+                updateHangmanImage()
+                checkWin()
             }
         }
-        print("Guesses so far: " + String(describing: guesses))
+        //print("Guesses so far: " + String(describing: guesses))
+        //print("Incorrect guesses so far: " + String(describing: incorrectGuesses))
+    }
+    
+    func checkWin(){
+        //win
+        var guessMatches = true
+        for char in answer.characters{
+            if char != " " && !guesses.contains(String(char)){
+                guessMatches = false
+            }
+        }
+        if(guessMatches){
+            //print("Game Won")
+            finishGameAlert(msg: "You Won!")
+        }
+
+        //lose
+        if(incorrectGuesses.count>=6){
+            //print("Game Lost")
+            finishGameAlert(msg: "You Lost!")
+        }
+    }
+    
+    func finishGameAlert(msg: String){
+        let alertController = UIAlertController(title: msg, message:
+            nil, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Play Again", style: UIAlertActionStyle.default,handler: restartGame))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func restartGame(alert: UIAlertAction!){
+        print("Game has been restarted.")
+        
+        let hangmanPhrases = HangmanPhrases()
+        // Generate a random phrase for the user to guess
+        let phrase: String = hangmanPhrases.getRandomPhrase()
+        
+        print("Answer is: " + phrase)
+        answer = phrase.uppercased();
+        updateWordLabel(result: answerToBlanks());
+        
+        guesses = [String]();
+        incorrectGuesses = [String]();
+        
+        updateWordLabel(result: answerToBlanks())
+        updateIncorrectGuessesLabel()
+        updateHangmanImage()
     }
 }
